2b612eea48e816282fdae9479898bfd3c5b1f8b4
Daniel Aristótelis Assunção da Silva <daniel.aristoteles@pwi.com.br>
2024-06-07 14:10:22 -0300

    Juntando componentes

--- doceria-bizus/src/ui/components/banner/index.js
+++ doceria-bizus/src/ui/components/banner/index.js
@@ -1,16 +1,18 @@
 import bannerImage from '../../../assets/pictures/IMG_20230915_095331.jpg'
-import { Image, SubTitle, Title, DivAlt, DivContent, ImageDiv, DivTitle } from './styled';
+import { Image, DivAlt, DivContent, ImageDiv, DivTitle } from './styled';
+
+import { Tittle, Text } from '../../styles/styledTexts';
 
 export default function Banner(){
     return (
         <DivAlt>
             <DivContent>
-                <DivTitle>
-                    <Title>Doceria Bizus</Title>
-                    <SubTitle>
+                <div className='tittle-container'>
+                    <Tittle size="120">Doceria Bizus</Tittle>
+                    <Text size="25" width="660px">
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-                    </SubTitle>
-                </DivTitle>
+                    </Text>
+                </div>
                 <ImageDiv>
                     <Image src={bannerImage} alt='Foto brigadeiros'/>
                 </ImageDiv>
